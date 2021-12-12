5891709ced98b7c069049694d940e0f3e6e9d745
Parth Patel <ppatel@uxteam.com>
2021-12-11 23:19:25 -0500

    Initial structure for main container, and blocks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <Header title="Amazon Cart" />
+      <div className="App-main container d-flex">
+        <div className='cartItems d-flex p-3'>
+          <h1>Shopping Cart</h1>
+        </div>
+        <div className='cartTotal d-flex p-3'>
+          
+        </div>
+      </div>
+
     </div>
   );
 }
